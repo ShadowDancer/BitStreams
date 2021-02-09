8fda9b6b3f8ca2421d4139802aa168615c279d9f
Przemysław Onak <przemekonak@gmail.com>
2021-02-09 21:37:58 +0100

    Update comments to better explain bit direction

--- BitStreams/BitDirection.cs
+++ BitStreams/BitDirection.cs
@@ -3,13 +3,13 @@
     public enum BitDirection
     {
         /// <summary>
-        ///     Start writing bits from MSB
+        ///     Start writing bits from Most significant bit aka left-most bit, high-order bit.
         /// </summary>
         /// <example>writing 1 to 00000000 equals in 10000000</example>
         MsbFirst = 0,
 
         /// <summary>
-        ///     Start writing bits from LSB
+        ///     Start writing bits from Least significant bit aka right-most bit, low-order bit.
         /// </summary>
         /// <example>writing 1 to 00000000 equals in 00000001</example>
         LsbFirst = 1
